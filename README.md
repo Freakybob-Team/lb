f978c07c8e8e05b4da1b8a1ea074a973254e965f
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-18 17:05:04 -0500

    new file:   README.md 	new file:   lb.py

---
+++ README.md
@@ -0,0 +1,9 @@
+# lb
+lb is a 'command prompt' made by the Freakybob Team!
+# commands
+fs: launches freakysay that echos ur words
+help: shows a list of commands
+credits: shows credits
+# credits
+Wish made the intial 1.0 version! He also made freakysay and the first version of help.
+Names helped Wish decide on the name!
---
+++ lb.py
@@ -0,0 +1,23 @@
+print("Welcome to LB, also known as LigmaBalls.")
+print("For help, type help below.")
+command = input("lb > ")
+if (command == "fs"):
+    isstop = "no"
+    print("Welcome to freakysay, also known as FS.")
+    print("To exit, say stop.")
+    while (isstop == "no"):
+        echo = input("freakysay < ")
+        if (echo == "stop"):
+            isstop = "yes"
+        print(echo)
+if (command == "help"):
+    print("Welcome to LigmaBalls!")
+    print("Here are some commands you can use:")
+    print("fs")
+    print("Using fs will launch freakysay, so you can echo your words!")
+    print("credits")
+    print("Using credits will show the people who helped make this!")
+if (command == "credits"):
+    print("Wish made the intial 1.0 version! He also made freakysay and the first version of help.")
+    print("Names helped Wish decide on the name!")
+    print("Made with love by the Freakybob Team <3")
