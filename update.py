83236998c55543ba23727c4b42c551d6897cfa17
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-20 08:12:20 -0500

    modified:   update.py

--- update.py
+++ update.py
@@ -1,8 +1,6 @@
 import requests
 import os
 print("Welcome to the Update Wizard. This will update your current LB version.")
-name = input("What is the name of your LigmaBalls file? (DO NOT INCLUDE .PY) ")
-os.remove(name + ".py")
 url = 'https://github.com/Freakybob-Team/lb/blob/main/lb.py?raw=true'
 response = requests.get(url)
 file_Path = "lb.py"
