97641e4eb36cb0b7a7c7b5168ba3cb1a0511cd02
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-20 07:56:21 -0500

    add alternative download source for updater (Codeberg) modified:   update.py

--- update.py
+++ update.py
@@ -11,6 +11,15 @@ if response.status_code == 200:
     with open(file_Path, 'wb') as file:
         file.write(response.content)
         print('File downloaded successfully')
+else:
+    print("Oops! There was an error and we couldn't download LigmaBalls.")
+    print("We're trying a different source...")
+    alturl = 'https://codeberg.org/Freakybob/lb/raw/branch/main/lb.py'
+    response = requests.get(alturl)
+    file_Path = "lb.py"
+    with open(file_Path, 'wb') as file:
+        file.write(response.content)
+        print('File downloaded successfully')
 print("Done! Your LigmaBalls version has been updated.")
 wantremove = input("Do you want to delete update.py? (Yes/No)")
 if (wantremove == "Yes"):
